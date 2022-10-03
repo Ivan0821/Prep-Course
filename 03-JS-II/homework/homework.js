0d9f61a59c19cde4bf8809d3b2f10726de961974
Ivan0821 <riverosivanagustin@gmail.com>
2022-10-02 22:50:40 -0300

    update hw-3 v1

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -44,12 +44,17 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero === 10 || numero === 5) {
+    return true;
+  }
+  return false;
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  
 }
 
 function esEntero(numero) {
